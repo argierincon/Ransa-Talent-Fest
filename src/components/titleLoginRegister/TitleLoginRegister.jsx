c86d679fbdd80ae850f4a3ec989336de7880ab3e
argierdfj <argierincon@gmail.com>
2020-10-12 18:52:40 -0500

    Añadiendo componente title login

---
+++ src/components/titleLoginRegister/TitleLoginRegister.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TitleLoginRegister = ({ title }) => <h3>{title}</h3>;
+
+TitleLoginRegister.propTypes = {
+  
+};
+
+export default TitleLoginRegister;
