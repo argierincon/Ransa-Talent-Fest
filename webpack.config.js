496b63c1b271b1828b77a163ddf70d8f688277eb
argierdfj <argierincon@gmail.com>
2020-10-12 15:02:14 -0500

    archivos de configuracion

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: [
+      '.js', '.jsx',
+    ],
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.html$/,
+      use: {
+        loader: 'html-loader',
+      },
+    },
+    {
+      test: /\.(css|scss|sass)$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      },
+      'css-loader',
+      'sass-loader',
+      ],
+    },
+    {
+      test: /\.(png|jpg|jpeg|gif|svg|)/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[hash].[ext]',
+        },
+      },
+    },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+};
