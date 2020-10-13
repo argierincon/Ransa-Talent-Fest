727c50758ea6d35cd34c5521d2a7cc3f5ac799d1
BBQ <erikaquispejanampa@gmail.com>
2020-10-13 00:44:15 -0500

    Actualización del router en la principal

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Login from './Views/login/Login';
-import Router from './router/Router'
+import Router from './router/Router';
 
 ReactDom.render(<Router />, document.getElementById('app'));
