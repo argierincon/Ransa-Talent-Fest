9adcdbe9cf052bbf3af17d85587a4449377782e8
argierdfj <argierincon@gmail.com>
2020-10-12 19:04:48 -0500

    añadiendo prop-types a la propiedad del componente

--- src/components/titleLoginRegister/TitleLoginRegister.jsx
+++ src/components/titleLoginRegister/TitleLoginRegister.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const TitleLoginRegister = ({ title }) => <h3>{title}</h3>;
 
 TitleLoginRegister.propTypes = {
-  
+  title: PropTypes.string.isRequired,
 };
 
 export default TitleLoginRegister;
