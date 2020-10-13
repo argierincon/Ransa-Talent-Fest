38e63266738bc32aa0ab192515c0beb4d2fdc2bc
BBQ <erikaquispejanampa@gmail.com>
2020-10-13 00:45:05 -0500

    Correción del componenre Login

--- src/Views/login/Login.jsx
+++ src/Views/login/Login.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import FormLogin from '../../components/FormLogin/FormLogin';
 
-const Login = () => {
-  return (
+const Login = () => (
   <div>
     <FormLogin />
   </div>
-  );
-}; 
-
+);
 
 export default Login;
