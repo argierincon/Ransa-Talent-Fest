08042c2d5cf0eb2f5d1db9cba22b2fc15759f8af
szapataq <szapata013@gmail.com>
2020-10-13 01:41:09 -0500

    Adición de banner a vista login

--- src/Views/login/Login.jsx
+++ src/Views/login/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import FormLogin from '../../components/FormLogin/FormLogin';
+import banner from '../../assets/img/banner.png';
 
 const Login = () => (
   <div>
     <FormLogin />
+    <div>
+      <img src={banner} alt="baner raven" />
+    </div>
   </div>
 );
 
