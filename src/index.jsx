eb7a011e857a124b0bf420d513c9dc8e6fb531bd
BBQ <erikaquispejanampa@gmail.com>
2020-10-13 00:27:19 -0500

    Actualización de componente origen

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Login from './Views/login/Login';
+import Router from './router/Router'
 
-ReactDom.render(<Login />, document.getElementById('app'));
+ReactDom.render(<Router />, document.getElementById('app'));
