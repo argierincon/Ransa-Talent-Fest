f201261bc7af2956c4bbceb5c6cf194f59346357
argierdfj <argierincon@gmail.com>
2020-10-12 21:54:59 -0500

    añadiento titulo de formulario

--- src/components/titleLoginRegister/TitleLoginRegister.jsx
+++ src/components/titleLoginRegister/TitleLoginRegister.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TitleLoginRegister = ({ title }) => <h3>{title}</h3>;
+const TitleLoginRegister = ({ titulo }) => <h3>{titulo}</h3>;
 
 TitleLoginRegister.propTypes = {
-  title: PropTypes.string.isRequired,
+  titulo: PropTypes.string.isRequired,
 };
 
 export default TitleLoginRegister;
