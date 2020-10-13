def348bc5e952d89f5f3e760dcc0092e8638b9a5
BBQ <erikaquispejanampa@gmail.com>
2020-10-13 00:30:02 -0500

    Prueba de ruteo vista Login

--- src/Views/login/Login.jsx
+++ src/Views/login/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import TitleLoginRegister from '../../components/titleLoginRegister/TitleLoginRegister';
 
-const Login = () => (
+const Login = () => {
+  return (
   <div>
     <TitleLoginRegister title="Iniciar Sesión" />
+    Iniciar Sesión
   </div>
-);
+  );
+}; 
+
 
 export default Login;
