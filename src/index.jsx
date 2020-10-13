deeb21f060005b704c2fe32ceec942cfaaeb623e
argierdfj <argierincon@gmail.com>
2020-10-12 21:54:23 -0500

    cambiando nombre de archivo de estilo global

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Login from './Views/login/Login';
+import './assets/styles/_variables.scss';
+// import './components/btnPrimary/BtnPrimary.scss';
 
 ReactDom.render(<Login />, document.getElementById('app'));
