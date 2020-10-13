7fd76338a5583291168ca900c23d6529a2394698
argierdfj <argierincon@gmail.com>
2020-10-12 22:04:53 -0500

    Añadiendo titulo de form al FormLogin

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './FormLogin.scss';
 import emailIcon from '../../assets/icons/email.svg';
+import BtnPrimary from '../btnPrimary/BtnPrimary';
 import passwordIcon from '../../assets/icons/password.svg';
 
 const FormLogin = () => (
   <div className="container-form">
     <form className="form-login">
+      <h3>Iniciar Sesión</h3>
       <div className="comun">
         <img src={emailIcon} className="icon-input" alt="email" />
         <input type="email" placeholder="Correo electrónico" />
@@ -14,6 +16,7 @@ const FormLogin = () => (
         <img src={passwordIcon} className="icon-input" alt="password" />
         <input type="password" placeholder="Contraseña" />
       </div>
+      <BtnPrimary texto="Iniciar Sesión" />
     </form>
   </div>
 );
