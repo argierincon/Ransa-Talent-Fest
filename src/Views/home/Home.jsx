dc5d6c209c7f1cb3c77b4135da0ab51fbea5201c
BBQ <erikaquispejanampa@gmail.com>
2020-10-13 00:29:49 -0500

    Prueba de ruteo vista Home

---
+++ src/Views/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../../components/titleLoginRegister/TitleLoginRegister';
+import TitleLoginRegister from '../../components/titleLoginRegister/TitleLoginRegister';
+
+const Login = () => {
+return (
+  <div>
+    <TitleLoginRegister title="prueba Home" />
+    prueba Home
+  </div>
+);
+};
+
+export default Login;
