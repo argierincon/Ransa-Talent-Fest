a8a8280358d3c4f892782aa1ddb3afa1b657622e
argierdfj <argierincon@gmail.com>
2020-10-14 09:50:26 -0500

    añadiendo vista unidades disponibles

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -8,6 +8,7 @@ import RequestDetail from '../Views/requestDetail/RequestDetail';
 import FormSolicitud from '../components/formSolicitud/FormSolicitud';
 import RequestStatus from '../Views/requestStatus/RequestStatus';
 import DriveEnablement from '../Views/driveEnablement/DriveEnablement';
+import UnitsAvailability from '../Views/unitsAvailability/UnitsAvailability';
 
 const Router = () => (
   <BrowserRouter>
@@ -16,9 +17,11 @@ const Router = () => (
       <Route exact path="/home" component={Home} />
       <Route exact path="/prueba" component={FormPrueba} />
       <Route exact path="/solicitudes" component={FormSolicitud} />
-      <Route exact path="/estatus-solicitud" component={RequestStatus} />
-      <Route exact path="/detalle-solicitud" component={RequestDetail} />
+      <Route exact path="/estatus-solicitudes" component={RequestStatus} />
+      <Route exact path="/detalle-solicitudes" component={RequestDetail} />
       <Route exact path="/habilitacion-unidades" component={DriveEnablement} />
+      <Route exact path="/disponibilidad-unidades" component={UnitsAvailability} />
+
     </Switch>
   </BrowserRouter>
 );
