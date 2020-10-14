dfc82719533acac0bbb181d91f487a26161caa3b
argierdfj <argierincon@gmail.com>
2020-10-14 00:00:28 -0500

    Terminando visat request status

--- src/Views/requestStatus/RequestStatus.jsx
+++ src/Views/requestStatus/RequestStatus.jsx
@@ -1,72 +1,94 @@
 import React from 'react';
+import Header from '../../components/header/Header';
+import TrafficLightRequest from '../../components/trafficLightRequest/TrafficLightRequest';
 import './RequestStatus.scss';
 
 const RequestStatus = () => (
-  <div className="seccion-estados-solicitud">
-    <h3>Solicitudes de Orden de Servicio</h3>
-    <div className="filtros-solicitud">
-      <p>Estatus</p>
-      <div>
-        <input type="date" />
-        <select name="empresa" id="empresa">
-          <option value="pacasmayo">Cementos Pacasmayo S.A.A</option>
-          <option value="marcobre">Marcobre S.A.C</option>
-          <option value="antamina">Compañia Minera Antamina S.A</option>
-          <option value="quenuales">E.M Los Quenuales</option>
-          <option value="ares">Compañia Minera Ares S.A.C</option>
-          <option value="yanacocha">Compañia Minera Ares S.A.C</option>
-        </select>
-        <select name="estado" id="estado">
-          <option value="pendiente">Pendiente</option>
-          <option value="enProceso">En proceso</option>
-          <option value="asignado">asignado</option>
-        </select>
+  <>
+    <Header />
+    <div className="seccion-estados-solicitud">
+      <h3>Solicitudes de Orden de Servicio</h3>
+      <div className="filtros-solicitud">
+        <p>Estatus</p>
+        <div>
+          <input className="width-height" type="date" />
+          <select className="width-height" name="empresa" id="empresa">
+            <option value="pacasmayo">Cementos Pacasmayo S.A.A</option>
+            <option value="marcobre">Marcobre S.A.C</option>
+            <option value="antamina">Compañia Minera Antamina S.A</option>
+            <option value="quenuales">E.M Los Quenuales</option>
+            <option value="ares">Compañia Minera Ares S.A.C</option>
+            <option value="yanacocha">Compañia Minera Ares S.A.C</option>
+          </select>
+          <select className="width-height" name="estado" id="estado">
+            <option value="pendiente">Pendiente</option>
+            <option value="enProceso">En proceso</option>
+            <option value="asignado">asignado</option>
+          </select>
+        </div>
+      </div>
+      <div className="tabla-estatus-solicitud">
+        <div className="titulos-tabla grid-tabla-solicitud">
+          <p>Fecha de carga</p>
+          <p>Hora de carga</p>
+          <p>Cliente</p>
+          <p>N° de Orden</p>
+          <p>Unidad</p>
+          <p>Mercadería</p>
+          <p>Fecha de entrega</p>
+          <p>Lugar de descarga</p>
+          <p> </p>
+        </div>
+        <div className="linea-bg grid-tabla-solicitud height-padding ">
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <TrafficLightRequest clase="solicitud-pendiente" estado="pendiente" />
+        </div>
+        <div className="linea-simple grid-tabla-solicitud height-padding ">
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <TrafficLightRequest clase="solicitud-pendiente" estado="pendiente" />
+        </div>
+        <div className="linea-bg grid-tabla-solicitud height-padding ">
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <TrafficLightRequest
+            clase="solicitud-en-proceso"
+            estado="En proceso"
+          />
+        </div>
+        <div className="linea-simple grid-tabla-solicitud height-padding ">
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <TrafficLightRequest clase="solicitud-asignada" estado="Asignado" />
+        </div>
       </div>
     </div>
-    <div className="tabla-estatus-solicitud">
-      <div className="titulos-tabla grid-tabla-solicitud">
-        <p>Fecha de carga</p>
-        <p>Hora de carga</p>
-        <p>Cliente</p>
-        <p>N° de Orden</p>
-        <p>Unidad</p>
-        <p>Mercadería</p>
-        <p>Fecha de entrega</p>
-        <p>Lugar de descarga</p>
-        <p> </p>
-      </div>
-      <div className="linea-bg grid-tabla-solicitud height-padding ">
-        <p>06/10/2020</p>
-        <p>8:00:00</p>
-        <p>2287 - CIA Minera Antamina</p>
-        <p>12345678</p>
-        <p>Cama baja</p>
-        <p>Diversos</p>
-        <p>06/10/2020</p>
-        <p>Antamina</p>
-      </div>
-      <div className="linea-simple grid-tabla-solicitud height-padding ">
-        <p>06/10/2020</p>
-        <p>8:00:00</p>
-        <p>2287 - CIA Minera Antamina</p>
-        <p>12345678</p>
-        <p>Cama baja</p>
-        <p>Diversos</p>
-        <p>06/10/2020</p>
-        <p>Antamina</p>
-      </div>
-      <div className="linea-bg grid-tabla-solicitud height-padding ">
-        <p>06/10/2020</p>
-        <p>8:00:00</p>
-        <p>2287 - CIA Minera Antamina</p>
-        <p>12345678</p>
-        <p>Cama baja</p>
-        <p>Diversos</p>
-        <p>06/10/2020</p>
-        <p>Antamina</p>
-      </div>
-    </div>
-  </div>
+  </>
 );
 
 export default RequestStatus;
