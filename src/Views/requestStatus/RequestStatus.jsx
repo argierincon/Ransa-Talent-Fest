b76224bcf8599801bd0e03b07f0cdb4e3ccfc075
argierdfj <argierincon@gmail.com>
2020-10-13 23:05:59 -0500

    añadiendo vista estado de solicitud

---
+++ src/Views/requestStatus/RequestStatus.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './RequestStatus.scss';
+
+const RequestStatus = () => (
+  <div className="seccion-estados-solicitud">
+    <h3>Solicitudes de Orden de Servicio</h3>
+    <div className="filtros-solicitud">
+      <p>Estatus</p>
+      <div>
+        <input type="date" />
+        <select name="empresa" id="empresa">
+          <option value="pacasmayo">Cementos Pacasmayo S.A.A</option>
+          <option value="marcobre">Marcobre S.A.C</option>
+          <option value="antamina">Compañia Minera Antamina S.A</option>
+          <option value="quenuales">E.M Los Quenuales</option>
+          <option value="ares">Compañia Minera Ares S.A.C</option>
+          <option value="yanacocha">Compañia Minera Ares S.A.C</option>
+        </select>
+        <select name="estado" id="estado">
+          <option value="pendiente">Pendiente</option>
+          <option value="enProceso">En proceso</option>
+          <option value="asignado">asignado</option>
+        </select>
+      </div>
+    </div>
+    <div className="tabla-estatus-solicitud">
+      <div className="titulos-tabla grid-tabla-solicitud">
+        <p>Fecha de carga</p>
+        <p>Hora de carga</p>
+        <p>Cliente</p>
+        <p>N° de Orden</p>
+        <p>Unidad</p>
+        <p>Mercadería</p>
+        <p>Fecha de entrega</p>
+        <p>Lugar de descarga</p>
+        <p> </p>
+      </div>
+      <div className="linea-bg grid-tabla-solicitud height-padding ">
+        <p>06/10/2020</p>
+        <p>8:00:00</p>
+        <p>2287 - CIA Minera Antamina</p>
+        <p>12345678</p>
+        <p>Cama baja</p>
+        <p>Diversos</p>
+        <p>06/10/2020</p>
+        <p>Antamina</p>
+      </div>
+      <div className="linea-simple grid-tabla-solicitud height-padding ">
+        <p>06/10/2020</p>
+        <p>8:00:00</p>
+        <p>2287 - CIA Minera Antamina</p>
+        <p>12345678</p>
+        <p>Cama baja</p>
+        <p>Diversos</p>
+        <p>06/10/2020</p>
+        <p>Antamina</p>
+      </div>
+      <div className="linea-bg grid-tabla-solicitud height-padding ">
+        <p>06/10/2020</p>
+        <p>8:00:00</p>
+        <p>2287 - CIA Minera Antamina</p>
+        <p>12345678</p>
+        <p>Cama baja</p>
+        <p>Diversos</p>
+        <p>06/10/2020</p>
+        <p>Antamina</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default RequestStatus;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Views/home/Home';
 import Login from '../Views/login/Login';
 import FormPrueba from '../components/formPrueba/FormPrueba';
+import RequestStatus from '../Views/requestStatus/RequestStatus';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route exact path="/Home" component={Home} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/prueba" component={FormPrueba} />
+      <Route exact path="/estatus" component={RequestStatus} />
     </Switch>
   </BrowserRouter>
 );
