97fb78b404222272632f7482190593d2afd65593
szapataq <szapata013@gmail.com>
2020-10-13 22:44:32 -0500

    Añadiendo formulario de solicitud

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Views/home/Home';
 import Login from '../Views/login/Login';
 import FormPrueba from '../components/formPrueba/FormPrueba';
+import FormSolicitud from '../components/formSolicitud/FormSolicitud';
 
 const Router = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Router = () => (
       <Route exact path="/" component={Login} />
       <Route exact path="/Home" component={Home} />
       <Route exact path="/prueba" component={FormPrueba} />
+      <Route exact path="/solicitudes" component={FormSolicitud} />
     </Switch>
   </BrowserRouter>
 );
