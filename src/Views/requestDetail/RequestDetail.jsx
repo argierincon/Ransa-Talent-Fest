77636964019f3a5911fa305d91c7b3cfa69002c2
argierdfj <argierincon@gmail.com>
2020-10-14 02:59:15 -0500

    añadiendo vista detalle de solicitud

---
+++ src/Views/requestDetail/RequestDetail.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import TrafficLightRequest from '../../components/trafficLightRequest/TrafficLightRequest';
+import './RequestDetail.scss';
+
+const RequestStatus = () => (
+  <>
+    <Header />
+    <div className="seccion-estados-solicitud">
+      <h3>Solicitudes de Orden de Servicio</h3>
+      <div className="filtros-solicitud">
+        <p>Requerimientos</p>
+        <div>
+          <input className="width-height inpunt-date" type="date" />
+          <select className="width-height" name="empresa" id="empresa">
+            <option value="pacasmayo">Cementos Pacasmayo S.A.A</option>
+            <option value="marcobre">Marcobre S.A.C</option>
+            <option value="antamina">Compañia Minera Antamina S.A</option>
+            <option value="quenuales">E.M Los Quenuales</option>
+            <option value="ares">Compañia Minera Ares S.A.C</option>
+            <option value="yanacocha">Compañia Minera Ares S.A.C</option>
+          </select>
+          <select className="width-height" name="estado" id="estado">
+            <option value="pendiente">Pendiente</option>
+            <option value="enProceso">En proceso</option>
+            <option value="asignado">asignado</option>
+            <option value="fallido">Fallido</option>
+          </select>
+        </div>
+      </div>
+      <div className="tabla-estatus-solicitud-detail">
+        <div className="titulos-tabla grid-tabla-solicitud-detail">
+          <p>Fecha de solicitud</p>
+          <p>Fecha de carga</p>
+          <p>Hora de carga</p>
+          <p>Cliente</p>
+          <p>N° de Orden</p>
+          <p>Unidad</p>
+          <p>Mercadería</p>
+          <p>Fecha de entrega</p>
+          <p>Lugar de descarga</p>
+          <p> </p>
+        </div>
+        <div className="linea-bg grid-tabla-solicitud-detail item-solic-detalle ">
+          <p>06/10/2020</p>
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <div className="status-ver-mas">
+            <TrafficLightRequest clase="solicitud-asignada" estado="Asignado" />
+            <div className="ver-mas">
+              <p>+</p>
+            </div>
+          </div>
+        </div>
+        <div className="linea-simple grid-tabla-solicitud-detail item-solic-detalle ">
+          <p>06/10/2020</p>
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <div className="status-ver-mas">
+            <TrafficLightRequest
+              clase="solicitud-pendiente"
+              estado="Pendiente"
+            />
+            <div className="ver-mas">
+              <p>+</p>
+            </div>
+          </div>
+        </div>
+        <div className="linea-bg grid-tabla-solicitud-detail item-solic-detalle ">
+          <p>06/10/2020</p>
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <div className="status-ver-mas">
+            <TrafficLightRequest
+              clase="solicitud-en-proceso"
+              estado="En proceso"
+            />
+            <div className="ver-mas">
+              <p>+</p>
+            </div>
+          </div>
+        </div>
+        <div className="linea-simple grid-tabla-solicitud-detail item-solic-detalle ">
+          <p>06/10/2020</p>
+          <p>06/10/2020</p>
+          <p>8:00:00</p>
+          <p>2287 - CIA Minera Antamina</p>
+          <p>12345678</p>
+          <p>Cama baja</p>
+          <p>Diversos</p>
+          <p>06/10/2020</p>
+          <p>Antamina</p>
+          <div className="status-ver-mas">
+            <TrafficLightRequest clase="solicitud-fallida" estado="Fallido" />
+            <div className="ver-mas">
+              <p>+</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default RequestStatus;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Views/home/Home';
 import Login from '../Views/login/Login';
 import FormPrueba from '../components/formPrueba/FormPrueba';
-import RequestStatus from '../Views/requestStatus/RequestStatus';
+import RequestDetail from '../Views/requestDetail/RequestDetail';
 import FormSolicitud from '../components/formSolicitud/FormSolicitud';
+import RequestStatus from '../Views/requestStatus/RequestStatus';
 
 const Router = () => (
   <BrowserRouter>
@@ -13,8 +14,9 @@ const Router = () => (
       <Route exact path="/" component={Login} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/prueba" component={FormPrueba} />
-      <Route exact path="/estatus" component={RequestStatus} />
       <Route exact path="/solicitudes" component={FormSolicitud} />
+      <Route exact path="/estatus-solicitud" component={RequestStatus} />
+      <Route exact path="/detalle-solicitud" component={RequestDetail} />
     </Switch>
   </BrowserRouter>
 );
