8a009ff5cbbec91e4fa74f8856799bd018ea9a7c
szapataq <szapata013@gmail.com>
2020-10-15 08:44:13 -0500

    correccion de linter

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
 import { SideBarData } from './SideBarData';
 import './Bar.scss';
-import { IconContext } from 'react-icons';
 import logo from '../../assets/img/LOGO_RANSA.png';
 import user from '../../assets/img/Moises.png';
 
@@ -17,22 +15,20 @@ function SideBar() {
     <>
       <IconContext.Provider value={{ color: '#1AAF42' }}>
         <div className="navbar">
-          <Link to="#" className="menu-bars">
-        
-          </Link>
+          <Link to="#" className="menu-bars" />
           <div className="header">
             <div className="contenedorUser">
               <Link to="/" className=""><img className="logo" src={user} alt="Logo User" /></Link>
-            <div className="user-text">Cristian Narciso</div>
+              <div className="user-text">Cristian Narciso</div>
             </div>
           </div>
 
           <nav className={LadoBarra ? 'nav-menu active' : 'nav-menu'}>
             <ul className="nav-menu-items" onClick={ViewSideBar}>
               <li className="navbar-toggle">
-              <div className="contenedorLogo">
-              <Link to="/" className=""><img className="logo" src={logo} alt="Logo Ransa" /></Link>
-            </div>
+                <div className="contenedorLogo">
+                  <Link to="/" className=""><img className="logo" src={logo} alt="Logo Ransa" /></Link>
+                </div>
               </li>
               {SideBarData.map((item, index) => (
                 <li key={index} className={item.clase}>
