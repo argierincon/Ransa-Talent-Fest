22094372897e712bd617f88b8a3537845ad9c999
szapataq <szapata013@gmail.com>
2020-10-15 20:51:25 -0500

    Actualizacion

--- src/Views/mainRequest/MainRequest.jsx
+++ src/Views/mainRequest/MainRequest.jsx
@@ -104,10 +104,9 @@ const MainRequest = () => {
     db.collection('solicitudes').add(datosSolicitud).then(() => { resetForm(); });
   };
 
-
   return (
     <>
-      <Header nombre="Cristian Narcizo" cargo="Supervisor de Operaciones" />
+      <Header nombre="Moises Carrillo" cargo="Supervisor de Operaciones" />
       <section className="main-container-solicitud">
         <TitleView texto="Solicitud de Requerimientos - Almacenes" />
         <form onSubmit={handleSubmit}>
