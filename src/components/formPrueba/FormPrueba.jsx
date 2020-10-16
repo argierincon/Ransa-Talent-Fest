442c88328b0147151430b0846e1f4672910efe5d
argierdfj <argierincon@gmail.com>
2020-10-16 12:31:47 -0500

    MODAL DETALLE ASIG

--- src/components/formPrueba/FormPrueba.jsx
+++ src/components/formPrueba/FormPrueba.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import firebase from 'firebase';
 import 'firebase/firestore';
 
+import './ModalDetalleAsignacion.scss';
+
 const FormPrueba = () => {
   const db = firebase.firestore();
 
@@ -32,35 +34,43 @@ const FormPrueba = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="placa"
-          name="placa"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          placeholder="tipo"
-          name="tipo"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          placeholder="revision tecnica"
-          name="revisionTecnica"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          placeholder="soat"
-          name="soat"
-          onChange={handleInputChange}
-        />
-        <button type="submit">ok</button>
-      </form>
+  // <div>
+  //   <form onSubmit={handleSubmit}>
+  //     <input
+  //       type="text"
+  //       placeholder="placa"
+  //       name="placa"
+  //       onChange={handleInputChange}
+  //     />
+  //     <input
+  //       type="text"
+  //       placeholder="tipo"
+  //       name="tipo"
+  //       onChange={handleInputChange}
+  //     />
+  //     <input
+  //       type="text"
+  //       placeholder="revision tecnica"
+  //       name="revisionTecnica"
+  //       onChange={handleInputChange}
+  //     />
+  //     <input
+  //       type="text"
+  //       placeholder="soat"
+  //       name="soat"
+  //       onChange={handleInputChange}
+  //     />
+  //     <button type="submit">ok</button>
+  //   </form>
+
+    <div classNamme="modal-detalle-asignacion">
+      <h3><span>ASIGNACIÓN ORDEN DE SERVICIO:</span></h3>
+      <div className="flex">
+        <p>Tipo de unidad</p>
+        <div className="box-data">dfdsf</div>
+      </div>
     </div>
+    // </div>
   );
 };
 
