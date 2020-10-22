8456f49b913291ecebe8585702bbe3b86df27fcf
BBQ <erikaquispejanampa@gmail.com>
2020-10-22 14:16:29 -0500

    Fijar color estático

--- src/components/sideBarOp/BarOp.jsx
+++ src/components/sideBarOp/BarOp.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import BarOpData from './BarOpData';
 import './BarOp.scss';
 import logo from '../../assets/img/LOGO_RANSA.png';
 
+let listIndex = 0;
+const { pathname } = window.location;
+const findIndex = BarOpData.findIndex(obj =>obj.ruta === pathname);
+listIndex = findIndex;
+
+const setActiveClase = (data) => {
+  listIndex = data;
+};
 const BarOp = () => (
   <IconContext.Provider value={{ color: '#1AAF42' }}>
     <nav className="nav-menu active">
@@ -16,14 +24,20 @@ const BarOp = () => (
             </Link>
           </div>
         </li>
-        {BarOpData.map((item, index) => (
-          <li key={index} className={item.clase}>
+        {BarOpData.map((item, index) => {
+          let className;
+          if(index !==1){
+             className = listIndex === index ? 'active':'';
+          }
+          return (
+            <li key={index} className={item.clase+' '+className} onClick = {() => setActiveClase(index)}>
             <Link to={item.ruta}>
               {item.icono}
               <span>{item.titulo}</span>
             </Link>
           </li>
-        ))}
+          )
+        })};
       </ul>
     </nav>
   </IconContext.Provider>
